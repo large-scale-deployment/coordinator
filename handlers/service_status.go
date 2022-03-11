0a3f38bc68c6be392ed0b5f9d120e2e6a07d1c75
wxw-matt <matt.wxw.adelaide@gmail.com>
2022-03-11 22:54:21 +1030

    add service_status_test

--- handlers/service_status.go
+++ handlers/service_status.go
@@ -4,6 +4,7 @@ import (
 	"lss/coordinator/models"
 	"lss/coordinator/services"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
@@ -14,17 +15,17 @@ type ReqServiceStatus struct {
 	Version      string `json:"version" form:"version" query:"version"` // Service version
 	Group        string `json:"group" form:"group" query:"group"`       // Deployment group
 	NodeName     string `json:"node_name" form:"node_name" query:"node_name"`
-	PodIp        string `json:"pod_id" form:"pod_id" query:"pod_id"`
+	PodIp        string `json:"pod_ip" form:"pod_ip" query:"pod_ip"`
 	PodName      string `json:"pod_name" form:"pod_name" query:"pod_name"`
 	PodNamespace string `json:"pod_namespace" form:"pod_namespace" query:"pod_namespace"`
 }
 
-type RequestHandler struct {
+type ServiceStatusHandler struct {
 	Registry *services.Registry
 }
 
 // Handler
-func (handler *RequestHandler) RegisterServiceStatus(c echo.Context) (err error) {
+func (handler *ServiceStatusHandler) Create(c echo.Context) (err error) {
 	ss := new(ReqServiceStatus)
 	if err = c.Bind(ss); err != nil {
 		return err
@@ -38,6 +39,6 @@ func (handler *RequestHandler) RegisterServiceStatus(c echo.Context) (err error)
 		PodName:      ss.PodName,
 		PodNamespace: ss.PodNamespace,
 	}
-	handler.Registry.ReportServiceStatus(serviceStatus)
-	return c.String(http.StatusOK, ss.Name)
+	ID := handler.Registry.ReportServiceStatus(serviceStatus)
+	return c.String(http.StatusCreated, strconv.FormatUint(uint64(ID), 10))
 }
---
+++ handlers/service_status_test.go
@@ -0,0 +1,54 @@
+package handlers
+
+import (
+	"lss/coordinator/models"
+	"lss/coordinator/services"
+	"net/http"
+	"net/http/httptest"
+	"regexp"
+	"strings"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var (
+	spacesRegexp = regexp.MustCompile(`[\n\t]`)
+	statusJSON   = spacesRegexp.ReplaceAllString(`{
+"name":"My Go Service",
+        "version":"1.0",
+        "group":"Group 11",
+        "node_name":"node name 1",
+        "pod_ip":"192.168.8.8",
+        "pod_name":"pod-name-xxx",
+        "pod_namespace":"default"
+    }`, "")
+)
+
+func TestRegisterServiceStatus(t *testing.T) {
+	// Setup
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/registry/statuses", strings.NewReader(statusJSON))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	m := &models.Models{DB: db}
+	m.AutoMigrate()
+	registry := &services.Registry{DB: m.DB}
+	h := &ServiceStatusHandler{Registry: registry}
+
+	// Assertions
+	if assert.NoError(t, h.Create(c)) {
+		assert.Equal(t, http.StatusCreated, rec.Code)
+		// assert.Equal(t, statusJSON, rec.Body.String())
+	}
+}
